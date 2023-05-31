f1e60ddfbfe110f519ed81c039b95d74d404080f
Fernando <nandinholkas@gmail.com>
2023-05-31 16:37:27 -0300

    acrescentando o Client e o ClientDTO

---
+++ src/main/java/com/flproject/taskdvclient/TaskdvclientApplication.java
@@ -0,0 +1,13 @@
+package com.flproject.taskdvclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdvclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdvclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flproject/taskdvclient/TaskdvclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flproject.taskdvclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskdvclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
